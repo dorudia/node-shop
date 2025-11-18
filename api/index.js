eb1d75b2fe649b1accd20e167a8572b6fff48526
Doru Diaconu <dorudia@Dorus-MacBook-Pro.local>
2025-11-18 20:11:54 +0200

    Prod setup.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,22 @@
-const { app, connectDB } = require("../app");
+// api/index.js
 const serverless = require("serverless-http");
+const { app } = require("../app");
+const mongoose = require("mongoose");
 
-connectDB().then(() => console.log("MongoDB connected"));
+let conn = null;
 
-module.exports.handler = serverless(app);
+async function connectDB() {
+  if (conn) return conn; // deja conectat
+  conn = await mongoose.connect(process.env.MONGODB_URL);
+  return conn;
+}
+
+module.exports.handler = async (req, res) => {
+  try {
+    await connectDB(); // asigură conexiunea
+    return serverless(app)(req, res);
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    res.status(500).send("Internal Server Error");
+  }
+};
